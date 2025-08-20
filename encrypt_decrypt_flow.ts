ab4b13b1f2e0da1446f411734a0a91d6637c6146
Nik Moiseienko <53253722+kindkitchen@users.noreply.github.com>
2025-08-20 14:43:04 +0200

    Create encrypt_decrypt_flow.ts

---
+++ encrypt_decrypt_flow.ts
@@ -0,0 +1,60 @@
+/**
+Simple demo that show how to generate crypt keys pair for encrypt / decrypt purpose.
+Among key generation, and their usage the import of public key is also showed.
+(The owner of the private key the only one who can read the cipher. So probably you may want to export public key to someone, who will create cipher.)
+**/
+
+
+const subtle = crypto.subtle;
+const NAME_OF_THE_ALGORITHM = "RSA-OAEP";
+const NAME_OF_THE_HASH = "SHA-256";
+const text_encoder = new TextEncoder();
+const text_decoder = new TextDecoder();
+const { privateKey, publicKey } = await subtle.generateKey(
+  {
+    name: NAME_OF_THE_ALGORITHM,
+    modulusLength: 2048,
+    publicExponent: new Uint8Array([1, 0, 1]),
+    hash: { name: NAME_OF_THE_HASH },
+  },
+  true,
+  ["encrypt", "decrypt"],
+);
+const public_key_to_export = await subtle.exportKey("jwk", publicKey);
+const imported_public_key = await subtle.importKey(
+  "jwk",
+  public_key_to_export,
+  {
+    name: NAME_OF_THE_ALGORITHM,
+    hash: NAME_OF_THE_HASH,
+  },
+  true,
+  ["encrypt"],
+);
+
+if (String(publicKey) == String(imported_public_key)) {
+  throw "Public key and it's imported version should be different";
+}
+
+const message = "Hello world!";
+const cipher = await subtle.encrypt(
+  NAME_OF_THE_ALGORITHM,
+  imported_public_key,
+  text_encoder.encode(message),
+);
+
+if (message !== text_decoder.decode(cipher)) {
+  throw "Encrypted message should be unreadable";
+}
+
+const decrypted = await subtle.decrypt(
+  NAME_OF_THE_ALGORITHM,
+  privateKey,
+  cipher,
+);
+
+if (
+  message !== text_decoder.decode(decrypted)
+) {
+  throw "Cipher should be decrypted to original message";
+}
